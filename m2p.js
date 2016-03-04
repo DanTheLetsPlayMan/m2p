e3d9dc3909ffa7a4c525e06f5ce1520b150fe174
zuerrdan <zuerrdan@students.zhaw.ch>
2016-03-04 19:34:14 +0100

    added css option

--- m2p.js
+++ m2p.js
@@ -1,6 +1,10 @@
 var markdownpdf = require("markdown-pdf"),
     fs = require("fs");
 
+var options = {
+    cssPath: 'C:\\nodeApps\\m2p\\markdown.css'
+};
+
 if (process.argv[2]) {
     var inputFile = process.argv[2];
     var outputFile = inputFile.replace('.md', '.pdf');
@@ -8,6 +12,6 @@ if (process.argv[2]) {
     throw new Error('please enter markdown file name');
 }
 
-markdownpdf().from(inputFile).to(outputFile, function () {
+markdownpdf(options).from(inputFile).to(outputFile, function () {
     console.log("Done");
 });
